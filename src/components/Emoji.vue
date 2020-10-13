f6356f1cb8002393be685ad88cacfe62259bd2da
DaehunGwak <rhkrgns56@naver.com>
2020-10-14 00:54:45 +0900

    빙글빙글 이모지

---
+++ src/components/Emoji.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="emoji">
+    <div id="emojiFirst">🥺</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.emoji {
+  font-size: 10rem;
+  text-align: center;
+}
+
+#emojiFirst {
+  display: inline-block;
+  padding-top: 2%;
+  animation-duration: 2s;
+  animation-name: spin;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+@keyframes spin {
+  100% { transform: rotate(360deg);}
+}
+</style>
